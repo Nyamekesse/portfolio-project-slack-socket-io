fbc2c7a398fda9d2e883983c7c81c1a4d33b9cef
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-06-05 23:55:16 +0000

    looped through all namespaces and dynamically updated the dom

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,22 @@
 const socket = io("http://localhost:9000");
-const socket2 = io("http://localhost:9000/admin");
+const messageForm = document.querySelector("#message-form");
+let namespaceDiv = document.querySelector(".namespaces");
+
+// listen for ns list which is a list of all the namespaces
+socket.on("nsList", (nsData) => {
+  namespaceDiv.innerHTML = "";
+  nsData.forEach((ns) => {
+    namespaceDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src=${ns.img} /></div>`;
+  });
+
+  // Add click listener for each namespace
+  Array.from(document.getElementsByClassName("namespace")).forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      const nsEndpoint = elem.getAttribute("ns");
+      console.log(`${nsEndpoint} I should go to now`);
+    });
+  });
+});
 
 socket.on("messageFromServer", (dataFromServer) => {
   console.log(dataFromServer);
@@ -9,12 +26,6 @@ socket.on("messageFromServer", (dataFromServer) => {
 socket.on("joined", (msg) => {
   console.log(msg);
 });
-
-socket2.on("welcome", (dataFromServer) => {
-  console.log(dataFromServer);
-});
-
-const messageForm = document.querySelector("#message-form");
 
 messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
